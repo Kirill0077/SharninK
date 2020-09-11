dc364d762c9152fa0b688441b325e81a907f9fdd
Kirill0077 <67799834+Kirill0077@users.noreply.github.com>
2020-09-11 16:28:09 +0300

    Добавьте файлы проекта.

---
+++ SharninK/SharninK.cpp
@@ -0,0 +1,18 @@
+﻿// SharninK.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int s, y;
+    //вывод фразы Hello world
+    cout << "Hello World!\n";
+    // ввод символа и его проверка
+    cout << "Vvedite simvol s";
+    cin >> s;
+    system("pause");
+    y = s * 2;
+    cout << "y=s*2\n" << "y=" << y;
+    return 0;
+}
